42cfe3c533b795c6e55723058341319c69f9cd3a
Caitlin Pratt <cpratt@swingleft.org>
2024-03-15 09:48:47 -0400

    Messages when pulling FDA data

--- clinicaltrials/api/fetch_fda_data.py
+++ clinicaltrials/api/fetch_fda_data.py
@@ -17,12 +17,12 @@ def make_fda_api_call(skip, limit=1000, start_date="2003-01-01", end_date="2024-
         - skip: (int) value initially sent to 0. used to paginate.
         - limit: (int) the maximum number of results that can be returned from
         each API query. The API sets this to 1000.
-        -start_date: (str) the start date of submission_status_date used in 
+        -start_date: (str) the start date of submission_status_date used in
         results. For the project, this is set to the start of 2003 to align with
         the clinical trials data (this is approximately when clinical trials
         data is first available from)
-        -end_date: (str) the end date of submission_status_date used in 
-        results. For the project, this is set to 2/19/2024 to align with our 
+        -end_date: (str) the end date of submission_status_date used in
+        results. For the project, this is set to 2/19/2024 to align with our
         final data collection.
 
     Returns:
@@ -51,27 +51,36 @@ def pull_fda_api_data(
         - skip: (int) value initially sent to 0. used to paginate.
         - limit: (int) the maximum number of results that can be returned from
         each API query. The API sets this to 1000.
-        -start_date: (str) the start date of submission_status_date used in 
+        -start_date: (str) the start date of submission_status_date used in
         results. For the project, this is set to the start of 2003 to align with
-        the clinical trials data (this is approximately when clinical trials 
+        the clinical trials data (this is approximately when clinical trials
         data is first available from)
-        -end_date: (str) the end date of submission_status_date used in 
-        results. For the project, this is set to 2/19/2024 to align with our 
+        -end_date: (str) the end date of submission_status_date used in
+        results. For the project, this is set to 2/19/2024 to align with our
         final data collection.
 
     Returns:
         - Writes data from the API out to a json file.
     """
+
     results = []
+    count_results = 0
+    next_results = count_results + limit
+
     while True:
+        print(f"Pulling FDA records {count_results} to {next_results}")
         time.sleep(2)
+
         apicall = make_fda_api_call(
             skip, limit=1000, start_date="2003-01-01", end_date="2024-02-19"
         )
+
         if apicall is None:
             break
 
         results.append(apicall)
+        count_results += limit
+        next_results += limit
         skip += limit
         write_data(results, "fda", append=False)
 
@@ -91,14 +100,15 @@ def write_data(data, source, append=True):
     """
 
     if append:
-        mode = 'a'
+        mode = "a"
     else:
-        mode = 'w'
+        mode = "w"
 
     pth = pathlib.Path(__file__).parent / f"../../data/{source}.json"
 
     with open(pth, mode=mode) as f:
         f.write(json.dumps(data))
+
 
 if __name__ == "__main__":
     pull_fda_api_data()
--- clinicaltrials/api/fetch_trials_data.py
+++ clinicaltrials/api/fetch_trials_data.py
@@ -7,20 +7,35 @@ import json
 import time
 import pathlib
 
-API_URL = 'https://www.clinicaltrials.gov/api/v2/studies'
+API_URL = "https://www.clinicaltrials.gov/api/v2/studies"
 
-API_FIELDS = ['NCTId', 'BriefTitle', 'OfficialTitle', 'Condition',
-              'StatusModule', 'InterventionName', 'InterventionOtherName',
-              'Phase', 'BriefSummary', 'Keyword', 'ArmGroupLabel',
-              'InterventionDescription', 'ArmGroupDescription',
-              'LeadSponsorName', 'LocationCity', 'LocationCountry',
-              'StudyType', 'BaselineCharacteristicsModule']
+API_FIELDS = [
+    "NCTId",
+    "BriefTitle",
+    "OfficialTitle",
+    "Condition",
+    "StatusModule",
+    "InterventionName",
+    "InterventionOtherName",
+    "Phase",
+    "BriefSummary",
+    "Keyword",
+    "ArmGroupLabel",
+    "InterventionDescription",
+    "ArmGroupDescription",
+    "LeadSponsorName",
+    "LocationCity",
+    "LocationCountry",
+    "StudyType",
+    "BaselineCharacteristicsModule",
+]
 
 
 def make_trials_api_call(
-    fields, limit_per_call,
-        pageToken=None,
-) -> {json, str}:
+    fields,
+    limit_per_call,
+    pageToken=None,
+):
     """
     Makes an API call to NIH's clinical trials API.
 
@@ -32,15 +47,17 @@ def make_trials_api_call(
         string value of the next page token
     """
 
-    fields = '|'.join(fields)
-    payload = {'fields': fields, 'query.intr': 'AREA[InterventionType]DRUG',
-               'pageToken': pageToken, 'pageSize': limit_per_call,
-               'postFilter.advanced':
-               'AREA[IsFDARegulatedDrug]true AND AREA[Phase](PHASE3 OR PHASE4)',
-               'query.locn': 'AREA[LocationCountry]United States'}
+    fields = "|".join(fields)
+    payload = {
+        "fields": fields,
+        "query.intr": "AREA[InterventionType]DRUG",
+        "pageToken": pageToken,
+        "pageSize": limit_per_call,
+        "postFilter.advanced": "AREA[IsFDARegulatedDrug]true AND AREA[Phase](PHASE3 OR PHASE4)",
+        "query.locn": "AREA[LocationCountry]United States",
+    }
 
-    r = requests.get(
-        'https://www.clinicaltrials.gov/api/v2/studies', params=payload)
+    r = requests.get("https://www.clinicaltrials.gov/api/v2/studies", params=payload)
 
     r.raise_for_status()
 
@@ -62,9 +79,9 @@ def write_data(data, source, append=True):
     """
 
     if append:
-        mode = 'a'
+        mode = "a"
     else:
-        mode = 'w'
+        mode = "w"
 
     pth = pathlib.Path(__file__).parent / f"../../data/{source}.json"
 
@@ -72,8 +89,7 @@ def write_data(data, source, append=True):
         f.write(json.dumps(data))
 
 
-def pull_trials_data(limit_per_call=1000, limit_total=float('inf'),
-                     fields=API_FIELDS):
+def pull_trials_data(limit_per_call=1000, limit_total=float("inf"), fields=API_FIELDS):
     """
     Pulls trials data, and writes it to a JSON file.
     Args:
@@ -82,21 +98,18 @@ def pull_trials_data(limit_per_call=1000, limit_total=float('inf'),
         limit_total (int): The maximum total number of records to return.
     """
 
-    print(
-        f"Making initial API call for records between 0 and {limit_per_call}"
-    )
+    print(f"Making initial API call for records between 0 and {limit_per_call}")
 
     # Make initial API call, grab next page token to handle in for loop
     results = []
 
-    response = make_trials_api_call(
-        limit_per_call=limit_per_call, fields=fields).json()
+    response = make_trials_api_call(limit_per_call=limit_per_call, fields=fields).json()
 
-    results = response['studies']
+    results = response["studies"]
 
     count_results = limit_per_call
 
-    next_page_token = response['nextPageToken']
+    next_page_token = response["nextPageToken"]
 
     while next_page_token and count_results + limit_per_call <= limit_total:
         time.sleep(2)
@@ -106,21 +119,23 @@ def pull_trials_data(limit_per_call=1000, limit_total=float('inf'),
 
             print(f"Pulling clinical trial records {count_results} to {next_results}")
 
-            response = make_trials_api_call(limit_per_call=limit_per_call,
-                        fields=fields, pageToken=next_page_token).json()
+            response = make_trials_api_call(
+                limit_per_call=limit_per_call, fields=fields, pageToken=next_page_token
+            ).json()
 
-            next_page_token = response['nextPageToken']
+            next_page_token = response["nextPageToken"]
 
-            results.extend(response['studies'])
+            results.extend(response["studies"])
 
             count_results += limit_per_call
 
         except KeyError:
             print("API pull complete.")
-            results.extend(response['studies'])
+            results.extend(response["studies"])
             break
 
-    write_data(results, 'trials', append=False)
+    write_data(results, "trials", append=False)
+
 
 if __name__ == "__main__":
     pull_trials_data()
